5bf5618173a5cf9047f2ed2fd986610c6ddda1e7
AldoLunaBueno <albanovaphi7@gmail.com>
2025-04-15 21:14:35 -0500

    Route for questions resource and database schema and model to pass the test

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-from fastapi import FastAPI
-import databases
+from fastapi import FastAPI # type: ignore
+import databases # type: ignore
+import os
+from pydantic import BaseModel # type: ignore
+import sqlalchemy  # type: ignore
+from typing import List
 
-DATABASE_URL = "postgresql://user:123@db:5432/triviadb"
-
+DATABASE_URL = os.getenv("DATABASE_URL",  "postgresql://user:123@db:5432/triviadb")
 db = databases.Database(DATABASE_URL)
+
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+questions = sqlalchemy.Table(
+    "questions",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("description", sqlalchemy.String),
+    sqlalchemy.Column("options", sqlalchemy.ARRAY(sqlalchemy.String)),
+    sqlalchemy.Column("correct_answer", sqlalchemy.String),
+    sqlalchemy.Column("difficulty", sqlalchemy.Integer)
+)
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.create_all(engine)
+
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
     await db.connect()
     print("Conected to postgresql")
-
 
 @app.on_event("shutdown")
 async def shutdown():
@@ -20,3 +39,20 @@ async def shutdown():
 @app.get("/")
 async def root():
     return {"message": "Hello from FastAPI!"}
+
+class QuestionCreate(BaseModel):
+    description: str
+    options: List[str]
+    correct_answer: int
+    difficulty: int
+    
+@app.get("/questions/", status_code=201)
+async def create_questions(question: QuestionCreate):
+    query = questions.insert().values(
+        description = question.description,
+        options = question.options,
+        correct_answer = question.correct_answer,
+        difficulty = questions.difficulty
+    )
+    await db.execute(query)
+    return {"message": "Question created"}
