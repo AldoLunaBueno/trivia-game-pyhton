da31b4b0a90b0f6dfb5c90e62e54d529b50c421e
AldoLunaBueno <albanovaphi7@gmail.com>
2025-04-15 19:30:44 -0500

    Using a test database instead of the real database and manage cleanup

--- test_api.py
+++ test_api.py
@@ -1,7 +1,23 @@
-from fastapi.testclient import TestClient
-from main import app
+import os
+os.environ["DATABASE_URL"] = "postgresql://user:123@db:5432/triviadb_test"
+
+
+from fastapi.testclient import TestClient # type: ignore
+from main import app, db, questions
+import asyncio
+import pytest
 
 client = TestClient(app)
+
+
+@pytest.fixture(autouse=True)
+def clear_table():
+    # Cleans up test database called triviadb_test
+    async def cleanup():
+        await db.connect()
+        await db.execute(questions.delete())
+        await db.disconnect()
+    asyncio.run(cleanup())
 
 def test_create_question():
     response = client.post("/questions/", json={
