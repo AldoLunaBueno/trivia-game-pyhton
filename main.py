576d19d0736639ede842ed1e581e7916e0aadf2e
AldoLunaBueno <albanovaphi7@gmail.com>
2025-04-16 02:04:32 -0500

    Passing the integration test, but solution is insecure and a migration is needed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI # type: ignore
 import databases # type: ignore
+from contextlib import asynccontextmanager
 import os
 from pydantic import BaseModel # type: ignore
 import sqlalchemy  # type: ignore
@@ -7,8 +8,6 @@ from typing import List
 
 DATABASE_URL = os.getenv("DATABASE_URL",  "postgresql://user:123@db:5432/triviadb")
 db = databases.Database(DATABASE_URL)
-
-database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 
 questions = sqlalchemy.Table(
@@ -17,24 +16,23 @@ questions = sqlalchemy.Table(
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("description", sqlalchemy.String),
     sqlalchemy.Column("options", sqlalchemy.ARRAY(sqlalchemy.String)),
-    sqlalchemy.Column("correct_answer", sqlalchemy.String),
+    sqlalchemy.Column("correct_answer", sqlalchemy.Integer),
     sqlalchemy.Column("difficulty", sqlalchemy.Integer)
 )
 
 engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.drop_all(engine)
 metadata.create_all(engine)
 
-app = FastAPI()
-
-@app.on_event("startup")
-async def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     await db.connect()
-    print("Conected to postgresql")
-
-@app.on_event("shutdown")
-async def shutdown():
+    print("Connected to PostgreSQL")
+    yield
     await db.disconnect()
-    print("Disconnected from postgresql")
+    print("Disconnected from PostgreSQL")
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
@@ -46,13 +44,16 @@ class QuestionCreate(BaseModel):
     correct_answer: int
     difficulty: int
     
-@app.get("/questions/", status_code=201)
+@app.post("/questions/", status_code=201)
 async def create_questions(question: QuestionCreate):
+    if not db.is_connected:
+        await db.connect()
+        
     query = questions.insert().values(
         description = question.description,
         options = question.options,
         correct_answer = question.correct_answer,
-        difficulty = questions.difficulty
+        difficulty = question.difficulty
     )
     await db.execute(query)
     return {"message": "Question created"}
